4d64b8950a1261c3c2c45a01ffc1d7f5d54ef421
Mohan <mohanjnv1@gmail.com>
2021-10-29 11:50:14 +0530

    Auth shared pref is working

---
+++ app/src/main/java/com/example/uidaiaddressupdate/Constants.java
@@ -0,0 +1,5 @@
+package com.example.uidaiaddressupdate;
+
+public class Constants {
+    public static String KEY_AUTH_TOKEN = "AuthToken";
+}
---
+++ app/src/main/java/com/example/uidaiaddressupdate/LandingActivity.java
@@ -0,0 +1,48 @@
+package com.example.uidaiaddressupdate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKeys;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.widget.Toast;
+
+import com.example.uidaiaddressupdate.auth.LoginActivity;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        try {
+            KeyGenParameterSpec keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC;
+            String mainKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec);
+            SharedPreferences sharedPreferences = EncryptedSharedPreferences.create(
+                    "aadharsharedPreferences",
+                    mainKeyAlias,
+                    this,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+            SharedPreferences.Editor sharedPrefsEditor = sharedPreferences.edit();
+            if(sharedPreferences.contains(Constants.KEY_AUTH_TOKEN)){
+                //send to home page
+                Toast.makeText(this, "Logged In", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LandingActivity.this,MainActivity.class));
+                finish();
+            }else{
+                //send to LoginPage
+                Toast.makeText(this, "Not logged in", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LandingActivity.this, LoginActivity.class));
+                finish();
+            }
+        }catch(Exception e){
+            Toast.makeText(this, "Error while reading shared pref", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
