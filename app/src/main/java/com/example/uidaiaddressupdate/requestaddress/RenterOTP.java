48679d8d62391a7d31f5c49f8d4a68e08ec4981c
Mohan <mohanjnv1@gmail.com>
2021-10-29 19:42:29 +0530

    landlord upload ekyc done

--- app/src/main/java/com/example/uidaiaddressupdate/requestaddress/RenterOTP.java
+++ app/src/main/java/com/example/uidaiaddressupdate/requestaddress/RenterOTP.java
@@ -1,4 +1,4 @@
-package com.example.uidaiaddressupdate;
+package com.example.uidaiaddressupdate.requestaddress;
 
 import android.os.Bundle;
 
@@ -8,11 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RenterOTP#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.uidaiaddressupdate.R;
+
+
 public class RenterOTP extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
--- app/src/main/java/com/example/uidaiaddressupdate/service/offlineekyc/model/ekycoffline/OfflineEkycXMLResponse.java
+++ app/src/main/java/com/example/uidaiaddressupdate/service/offlineekyc/model/ekycoffline/OfflineEkycXMLResponse.java
@@ -18,4 +18,36 @@ public class OfflineEkycXMLResponse{
     public void seteKycXML(String eKycXML) {
         this.eKycXML = eKycXML;
     }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(String requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getUidNumber() {
+        return uidNumber;
+    }
+
+    public void setUidNumber(String uidNumber) {
+        this.uidNumber = uidNumber;
+    }
 }
